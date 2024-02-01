5136673d3e4275b230b03ad1d049d73ab89fa95b
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-01 01:01:12 -0300

    new version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuTitleComponent } from "./componentes/menu-title/menu-title.component";
+import { MenuBarComponent } from "./componentes/menu-bar/menu-bar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MenuTitleComponent]
+    imports: [RouterOutlet, MenuTitleComponent, MenuBarComponent]
 })
 export class AppComponent {
   title = 'blog';
