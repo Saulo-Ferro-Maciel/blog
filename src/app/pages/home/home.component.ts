bc8ddc1d91be7846704b6d38c10be5ecda650bc8
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-01 22:37:57 -0300

    new version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MenuTitleComponent } from "../../componentes/menu-title/menu-title.component";
+import { MenuBarComponent } from "../../componentes/menu-bar/menu-bar.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [MenuTitleComponent, MenuBarComponent]
 })
 export class HomeComponent {
 
