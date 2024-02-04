64d74fbef78f951a2ef8d889ca00d90d8c0e4029
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-04 01:52:00 -0300

    new update

---
+++ src/app/components/footer-rodape/footer-rodape.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-rodape',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-rodape.component.html',
+  styleUrl: './footer-rodape.component.css'
+})
+export class FooterRodapeComponent {
+
+}
