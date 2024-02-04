93c5af86bb22e0309d63dc48138a06611177b007
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-04 03:07:31 -0300

    new update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,13 +5,14 @@ import { BigCardComponent } from "../../componentes/big-card/big-card.component"
 import { SmallCardComponent } from "../../componentes/small-card/small-card.component";
 import { ProjetosFrontendComponent } from "../../componentes/projetos-frontend/projetos-frontend.component";
 import { ProjetosBackendComponent } from "../../componentes/projetos-backend/projetos-backend.component";
+import { FooterRodapeComponent } from "../../componentes/footer-rodape/footer-rodape.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [MenuTitleComponent, MenuBarComponent, BigCardComponent, SmallCardComponent, ProjetosFrontendComponent, ProjetosBackendComponent]
+    imports: [MenuTitleComponent, MenuBarComponent, BigCardComponent, SmallCardComponent, ProjetosFrontendComponent, ProjetosBackendComponent, FooterRodapeComponent]
 })
 export class HomeComponent {
 
