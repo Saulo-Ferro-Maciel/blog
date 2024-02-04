da1ad7870b05a402a951b5e4d6fe675c7b635779
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-04 01:38:02 -0300

    new update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,15 @@ import { MenuTitleComponent } from "../../componentes/menu-title/menu-title.comp
 import { MenuBarComponent } from "../../componentes/menu-bar/menu-bar.component";
 import { BigCardComponent } from "../../componentes/big-card/big-card.component";
 import { SmallCardComponent } from "../../componentes/small-card/small-card.component";
+import { ProjetosFrontendComponent } from "../../componentes/projetos-frontend/projetos-frontend.component";
+import { ProjetosBackendComponent } from "../../componentes/projetos-backend/projetos-backend.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [MenuTitleComponent, MenuBarComponent, BigCardComponent, SmallCardComponent]
+    imports: [MenuTitleComponent, MenuBarComponent, BigCardComponent, SmallCardComponent, ProjetosFrontendComponent, ProjetosBackendComponent]
 })
 export class HomeComponent {
 
