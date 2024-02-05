1bc9718e0dc5ac19c220d5346c12a80ede382963
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-05 03:22:20 -0300

    New Update:
    	new file:   src/app/componentes/carrossel/carrossel.component.css
    	new file:   src/app/componentes/carrossel/carrossel.component.html
    	new file:   src/app/componentes/carrossel/carrossel.component.ts
    	modified:   src/app/componentes/projetos-frontend/projetos-frontend.component.html
    	modified:   src/app/componentes/projetos-frontend/projetos-frontend.component.ts

--- src/app/componentes/projetos-frontend/projetos-frontend.component.ts
+++ src/app/componentes/projetos-frontend/projetos-frontend.component.ts
@@ -1,11 +1,12 @@
 import { Component} from '@angular/core';
+import { CarrosselComponent } from "../carrossel/carrossel.component";
 
 @Component({
-  selector: 'app-projetos-frontend',
-  standalone: true,
-  imports: [],
-  templateUrl: './projetos-frontend.component.html',
-  styleUrl: './projetos-frontend.component.css'
+    selector: 'app-projetos-frontend',
+    standalone: true,
+    templateUrl: './projetos-frontend.component.html',
+    styleUrl: './projetos-frontend.component.css',
+    imports: [CarrosselComponent]
 })
 export class ProjetosFrontendComponent {
 
