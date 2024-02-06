89d0dc6f86e03ed1f808d767687686bb1bc9e3e2
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-06 14:39:40 -0300

    New Update:
    	deleted:    src/app/componentes/carrossel/carrossel.component.css
    	deleted:    src/app/componentes/carrossel/carrossel.component.html
    	deleted:    src/app/componentes/carrossel/carrossel.component.ts
    	modified:   src/app/componentes/projetos-frontend/projetos-frontend.component.html
    	modified:   src/app/componentes/projetos-frontend/projetos-frontend.component.ts
    	modified:   src/app/pages/home/home.component.html

--- src/app/componentes/projetos-frontend/projetos-frontend.component.ts
+++ src/app/componentes/projetos-frontend/projetos-frontend.component.ts
@@ -1,12 +1,12 @@
 import { Component} from '@angular/core';
-import { CarrosselComponent } from "../carrossel/carrossel.component";
+
 
 @Component({
     selector: 'app-projetos-frontend',
     standalone: true,
     templateUrl: './projetos-frontend.component.html',
     styleUrl: './projetos-frontend.component.css',
-    imports: [CarrosselComponent]
+    imports: []
 })
 export class ProjetosFrontendComponent {
 
