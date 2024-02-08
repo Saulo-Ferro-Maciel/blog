1b718268eac33640f8c87b617e1e3ef37693a0b7
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-08 00:48:03 -0300

    New Update:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/componentes/carousel/carousel.component.css
    	modified:   src/app/componentes/carousel/carousel.component.html#	modified:   src/app/componentes/carousel/carousel.component.ts

--- src/app/componentes/carousel/carousel.component.ts
+++ src/app/componentes/carousel/carousel.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SlickCarouselModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
 
+  slides = [
+    {img: "./assets/foto2.jpg"},
+    {img: "./assets/imagem_de_test.png"},
+    {img: "./assets/foto2.jpg"}
+  ];
+
+  slideConfig = {
+    "slidesToShow":4,
+    "slidesToScroll":4,
+    "autoplay": true,
+    "autoplaySpeed": 5000,
+    "pauseOnHover": true,
+    "infinite": true,
+    "responsive": [
+      {
+        "breakpoint":992,
+        "settings": {
+          "arrows": true,
+          "infinite":true,
+          "slidesToShow":3,
+          "slidesToScroll":3
+        }
+      },
+      {
+        "breakpoint":768,
+        "settings": {
+          "arrows": true,
+          "infinite":true,
+          "slidesToShow":1,
+          "slidesToScroll":1
+        }
+      }
+    ]
+  };
 }
