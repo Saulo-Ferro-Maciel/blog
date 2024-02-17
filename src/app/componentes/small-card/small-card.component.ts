9c1aea4dd990cedc2adabe35720b06d4930002d6
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-17 02:07:05 -0300

    new versio:
    	modified:   src/app/componentes/small-card/small-card.component.ts

--- src/app/componentes/small-card/small-card.component.ts
+++ src/app/componentes/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -9,8 +9,12 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
   
+  @Input()
   photoCover:string = ""
+  @Input()
   cardTitle:string = ""
+  @Input()
   cardTopic:string = ""
+  @Input()
   cardDate:string = ""
 }
