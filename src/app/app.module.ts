18aa2d009bbad727720288a280c9d76c0486d88a
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-29 03:27:56 -0300

    New version: 	new file:   src/app/app.module.ts 	modified:   src/app/app.routes.ts 	modified:   src/index.html

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component'; 
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { AbouteMeComponent } from './pages/aboute-me/aboute-me.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ProjectsComponent,
+    ArticlesComponent,
+    AbouteMeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
+
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'; 
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { AbouteMeComponent } from './pages/aboute-me/aboute-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'articles', component: ArticlesComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'about-me', component: AbouteMeComponent },
+  { path: '**', redirectTo: '/404' }
+];
