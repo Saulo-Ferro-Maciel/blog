4e1bf9ba01ce8e49ea7cb13886f4a85810abe30e
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-29 04:39:45 -0300

    .

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MenuBarComponent } from "../../componentes/menu-bar/menu-bar.component";
+import { FooterRodapeComponent } from "../../componentes/footer-rodape/footer-rodape.component";
 
 @Component({
-  selector: 'app-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+    selector: 'app-not-found',
+    standalone: true,
+    templateUrl: './not-found.component.html',
+    styleUrl: './not-found.component.css',
+    imports: [MenuBarComponent, FooterRodapeComponent]
 })
 export class NotFoundComponent {
 
