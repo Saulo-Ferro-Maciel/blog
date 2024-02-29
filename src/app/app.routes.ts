6c31906c88efcf3a7589ac437b12e51e3198cd46
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-02-29 04:39:01 -0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
 import { AbouteMeComponent } from './pages/aboute-me/aboute-me.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'articles', component: ArticlesComponent},
   { path: 'projects', component: ProjectsComponent},
   { path: 'about-me', component: AbouteMeComponent },
-  { path: '**', redirectTo: '/404' }
+  { path: '404', component: NotFoundComponent  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404'}
 ];
