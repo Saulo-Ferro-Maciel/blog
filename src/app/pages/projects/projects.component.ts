d1bd07197ba9b2ba6c54c774d0cd6ffb59adca63
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-03-09 14:55:48 -0300

    New Update:
    	modified:   src/app/componentes/footer-rodape/footer-rodape.component.css
    	modified:   src/app/pages/articles/articles.component.css
    	modified:   src/app/pages/home/home.component.html
    	modified:   src/app/pages/projects/projects.component.css
    	modified:   src/app/pages/projects/projects.component.html
    	modified:   src/app/pages/projects/projects.component.ts

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuBarComponent } from "../../componentes/menu-bar/menu-bar.component";
 import { FooterRodapeComponent } from "../../componentes/footer-rodape/footer-rodape.component";
+import { BigCardComponent } from "../../componentes/big-card/big-card.component";
 
 @Component({
     selector: 'app-projects',
     standalone: true,
     templateUrl: './projects.component.html',
     styleUrl: './projects.component.css',
-    imports: [MenuBarComponent, FooterRodapeComponent]
+    imports: [MenuBarComponent, FooterRodapeComponent, BigCardComponent]
 })
 export class ProjectsComponent {
 
